7f35f213f6ebe083e3abf16cd46d215e1b2fed87
Fyardlest <fyardlest1@gmail.com>
2020-07-13 23:02:42 -0400

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,6 +5,7 @@ import CampsiteInfo from "./CampsiteInfoComponent";
 import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Reservation from "./ReservationComponent";
+import Favorites from './FavoritesComponent';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from "react-native";
 import { createStackNavigator, createDrawerNavigator, DrawerItems } from "react-navigation";
 import { Icon } from 'react-native-elements';
@@ -143,6 +144,31 @@ const ReservationNavigator = createStackNavigator(
   }
 );
 
+const FavoritesNavigator = createStackNavigator(
+    {
+        Favorites: { screen: Favorites },
+    },
+    {
+        navigationOptions: ({ navigation }) => ({
+            headerStyle: {
+                backgroundColor: "#5637DD",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+                color: "#fff",
+            },
+            headerLeft: (
+                <Icon
+                    name='heart'
+                    type='font-awesome'
+                    iconStyle={StyleSheet.stackIcon}
+                    onPress={() => navigation.toggleDrawer()}
+                />
+            ),
+        }),
+    }
+);
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView
@@ -202,6 +228,20 @@ const MainNavigator = createDrawerNavigator (
                     />
                 )
             }    
+        },
+        Favorites: {
+            screen: FavoritesNavigator,
+            navigationOptions: {
+                drawerLabel: 'My Favorites',
+                drawerIcon: ({ tintColor }) => (
+                    <Icon
+                        name='heart'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
         },
         About: { 
             screen: AboutNavigator,
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = "http://192.168.1.101:3001/";
+export const baseUrl = "http://192.168.0.103:3001/";
