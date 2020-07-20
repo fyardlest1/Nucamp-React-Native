1d27bc3ef834b203048059d001e021b4a679c162
Fyardlest <fyardlest1@gmail.com>
2020-07-20 16:54:38 -0400

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -12,6 +12,8 @@ import {
 } from "react-native";
 import DatePicker from "react-native-datepicker";
 import * as Animatable from "react-native-animatable";
+import * as Permissions from "expo-permissions";
+import { Notifications } from "expo";
 
 class Reservation extends Component {
   constructor(props) {
@@ -55,8 +57,8 @@ class Reservation extends Component {
         {
           text: "Ok",
           onPress: () => {
+            this.presentLocalNotification(this.state.date);
             this.resetForm();
-            console.log("Ok pressed");
           },
         },
       ],
@@ -72,6 +74,28 @@ class Reservation extends Component {
       showModal: false,
     });
   }
+
+  async obtainNotificationPermission() {
+      const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+      if (permission.status !== 'granted') {
+        const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+          Alert.alert('Permission not granted to show notification');
+        }
+        return permission;
+      }
+      return permission;
+  }
+
+  async presentLocalNotification(date) {
+        const permission = await this.obtainNotificationPermission();
+        if (permission.status === 'granted') {
+            Notifications.presentLocalNotificationAsync({
+                title: 'Your Campsite Reservation Search',
+                body: 'Search for ' + date + ' requested'
+            });
+        }
+    }
 
   render() {
     return (
