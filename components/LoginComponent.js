a5825a8c2b3c44dc2e275d3817ffe1971a003d84
Fyardlest <fyardlest1@gmail.com>
2020-07-23 11:45:28 -0400

    Task 1 Assignmet Week4

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -4,6 +4,7 @@ import { Input, CheckBox, Button, Icon } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
+import * as ImageManipulator from 'expo-image-manipulator';
 import { createBottomTabNavigator } from 'react-navigation';
 import { baseUrl } from '../shared/baseUrl';
 
@@ -157,9 +158,21 @@ class RegisterTab extends Component {
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri});
+                this.processImage(capturedImage.uri);
             }
         }
+    }
+
+    processImage = async (imgUri) => {
+        let processedImage = await ImageManipulator.manipulateAsync(
+            imgUri,
+            [
+              { resize: { width: 400 } }
+            ],
+            { format: 'png' }
+          );
+        console.log(processedImage);
+        this.setState({ imageUrl: processedImage.uri });
     }
 
     handleRegister() {
